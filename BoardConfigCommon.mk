b712d8fe21ac4387cf289f1ba1b32ce6ee238fcc
Nathan Chancellor <natechancellor@gmail.com>
2018-04-11 15:13:23 -0700

    oneplus: msm8998: Compile the kernels with Clang
    Change-Id: I25aa3c517f4431b4c337d0a932660589c6c0aa7e
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
